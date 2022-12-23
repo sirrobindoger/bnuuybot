364342044f44b75829f84a66eea6566d34c90944
Sirrobindoger <solomonortegais@gmail.com>
2022-12-23 13:47:50 -0500

    delete after 1 minute

--- events/vcjoin.js
+++ events/vcjoin.js
@@ -7,14 +7,18 @@ const OnVCJoin = {
      * @param {VoiceState} oldState 
      * @param {VoiceState} newState 
      */
-    ON_FIRE: (oldState, newState) => {
+    ON_FIRE: async (oldState, newState) => {
         console.log("Voice State Update: ");
         console.log(oldState);
         console.log(newState);
         // announce to the guild in main-thread chat that a user has joined a voice channel
         // only announce if the voice channel was previously empty
         if (newState?.channel?.members.size === 1) {
-            newState.guild.channels.cache.find(c => c.name === "main-thread").send(`${newState.member.nickname} has joined ${newState.channel.name}!`);
+            const msg = await newState.guild.channels.cache.find(c => c.name === "main-thread").send(`${newState.member.nickname} has joined ${newState.channel.name}!`);
+            // delete the message after 1 minute
+            setTimeout(() => {
+                msg.delete();
+            } , 1000 * 60);
         }
     }
 }
